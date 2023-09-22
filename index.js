7eda36d5e0b37af7125352c4c76e46ee165d76a2
Shivanshu <pshivanshu99@gmail.com>
2023-09-22 17:08:27 +0530

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const app = express();
-const dbConnect  = require('./config/database')
-const router = require('./routes/routes');
 
-app.get('/',(req,res)=>{
-      res.send(`<h1>Hello world</h1>`)
-})
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+//cookie-parser - what is this and why we need this ?
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.use(express.json());
 
-// Setting up the router..
-app.use("/api/v1",router);
+require("./config/db").connect();
 
-app.listen(3000,()=>{
-      console.log("Hello App is running on 3000");
+//route import and mount
+const user = require("./route/route");
+app.use("/api/v1", user);
+
+//activate
+
+app.listen(PORT, () => {
+    console.log(`App is listening at ${PORT}`);
 })
-dbConnect();
